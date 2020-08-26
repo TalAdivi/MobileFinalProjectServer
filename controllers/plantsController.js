692335aa0bebf1aa111cf21aac19d62e94e190e4
tomerbar44 <48451037+tomerbar44@users.noreply.github.com>
2020-08-26 10:48:14 +0300

    Update plantsController.js

--- controllers/plantsController.js
+++ controllers/plantsController.js
@@ -7,8 +7,7 @@ const getItems = async (req, res) => {
     // bring weekly weather api results
     const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${req.params.lat}&lon=${req.params.lon}&exclude="current,hourly,minutely"&appid=5b28e3c836da5db352b2a9441c1354e4&units=metric`)
     const dbresult = await plantsModel.findByGrowItLogic(data,req.params.type)
-    res.status(200).send("check");
-    // res.status(200).json({ dbresult })
+    res.status(200).json({ dbresult })
   } catch (e) {
     res.status(500).json({
       status: 500,
@@ -22,7 +21,8 @@ const getItems = async (req, res) => {
 const getTypes = async (req, res) => {
   try {
     const dbresult = await plantsModel.findGrowItTypes()
-    res.status(200).json({ dbresult })
+    // res.status(200).json({ dbresult })
+    res.status(200).send("check");
   } catch (e) {
     res.status(500).json({
       status: 500,
