3f4edc3fed7a810a97352fc598f639dd4cca0b1b
Tal Adivi <39825719+TalAdivi@users.noreply.github.com>
2020-10-01 09:30:05 +0300

    changed avg factor

--- controllers/plantsController.js
+++ controllers/plantsController.js
@@ -22,9 +22,6 @@ const getTypes = async (req, res) => {
   try {
     const dbresult = await plantsModel.findGrowItTypes()
     res.status(200).json({ dbresult })
-    // res.setHeader('Content-Type','text/html')
-    // res.status(200).send('File not found')
-    // res.status(404).end("check")
   } catch (e) {
     res.status(500).json({
       status: 500,
